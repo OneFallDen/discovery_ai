51bfa37b671e056d84b34389a32458c57c597e51
Aleksandr Poluyan <onefallden@gmail.com>
2026-04-09 19:53:54 +0300

    feat(generation): added bullmq queue

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CommonModule } from "./modules/Common/CommonModule";
 import { ImageGenerationModule } from "./modules/ImageGeneration/ImageGenerationModule";
+import { BullModule } from "@nestjs/bull";
 
 @Module({
-    imports: [CommonModule, ImageGenerationModule],
+    imports: [
+        CommonModule,
+        ImageGenerationModule,
+        BullModule.forRoot({
+            redis: { host: "discovery-ai-redis", port: 6379 },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- backend/src/modules/ImageGeneration/ImageGenerationModule.ts
+++ backend/src/modules/ImageGeneration/ImageGenerationModule.ts
@@ -1,6 +1,14 @@
 import { Module } from "@nestjs/common";
+import { CommonModule } from "../Common/CommonModule";
+import { BullModule } from "@nestjs/bull";
 
 @Module({
+    imports: [
+        CommonModule,
+        BullModule.registerQueue({
+            name: "image_queue",
+        }),
+    ],
     providers: [],
     exports: [],
 })
